f78b8caa2d73fab3dcbf53d7066c312a2d3d6439
Yunkon (Alvin) Kim <hermitkim1@users.noreply.github.com>
2020-09-03 21:17:15 +0900

    Create calc_test.go

---
+++ calc_test.go
@@ -0,0 +1,14 @@
+package calc_test
+ 
+import (
+    "calc"
+    "testing"
+)
+ 
+func TestSum(t *testing.T) {
+    s := calc.Sum(1, 2, 3)
+ 
+    if s != 6 {
+        t.Error("Wrong result")
+    }
+}
